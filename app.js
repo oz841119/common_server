9676daf66bfa88b59f337d6d1fc047dd6190d6a1
YiliChen <yili@YiliChendeMacBook-Air.local>
2023-01-16 01:17:57 +0800

    add route: 5sec

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.send('Hi')
 })
 
+app.get('/5sec', (req, res) => {
+    setTimeout(() => {
+        res.send('5 sec timeout')
+    }, 5000);
+})
+
 app.listen(port, () => {
     console.log('Server Start');
 })
